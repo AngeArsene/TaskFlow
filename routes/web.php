cf5ef8b8b027b6e2c10f5bccc800fe0b40e11f7e
Ange Arsene <nkenmandenga@gmail.com>
2025-07-12 13:58:01 +0100

    Refactor route documentation for clarity and organization

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\ProjectController;
 
+/**
+ * Route for displaying the list of tasks.
+ */
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+
+/**
+ * Routes for task management.
+ */
 Route::resource('tasks', TaskController::class)->except(['show', 'index']);
 Route::resource('projects', ProjectController::class)->only(['store', 'destroy', 'update']);
+
+/**
+ * Route for reordering tasks.
+ */
 Route::post('/tasks/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
