3f5014add5907396d36e709096567d6ab4f43d8c
Ange Arsene <nkenmandenga@gmail.com>
2025-07-15 15:46:23 +0100

    Refactor App component declaration to use React.FC type

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -10,12 +10,13 @@ import { PageProps, Task, Project } from '@/types/index';
 import { ProjectManager } from '../components/ProjectManager';
 import { ProjectSelector } from '../components/ProjectSelector';
 
-function App() {
-  type Tasks = {
-    tasks: Task[],
-    projects: Project[],
-    selectedProject: number|null
-  };
+type Tasks = {
+  tasks: Task[],
+  projects: Project[],
+  selectedProject: number|null
+};
+
+const App: React.FC = () => {
 
   const {
     selectedProject,
